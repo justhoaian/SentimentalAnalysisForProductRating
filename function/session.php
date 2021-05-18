c830185e9fa49db23dc9027e2ae325995fd40f45
sarah-phan <72535562+sarah-phan@users.noreply.github.com>
2021-05-18 10:12:49 +0700

    delete

---
+++ function/session.php
@@ -0,0 +1,9 @@
+<?php
+session_start();
+
+//if the user is already logged in then redirect user to home page
+if ((isset($_SESSION["$username"])) && ($_SESSION["username"] === true)){
+    header("location: index.php");
+    exit;
+}
+?>
